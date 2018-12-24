5a6ef0ecb77c94ac519c7dc6246e75d75787cedb
Ubuntu <ubuntu@ip-172-31-18-175.ap-south-1.compute.internal>
2018-12-24 06:56:00 +0000

    added new changes

--- login.java
+++ login.java
@@ -1,5 +1,23 @@
 public void main()
 {
 system.out.println("welcome java");
-system.out.println("hi")
+
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+system.out.println("hi");
+
 }
